9c8484915e2324f7b1065b274059b5e6e4108bdc
tomasmigone <tomasmigone@gmail.com>
2014-12-13 18:56:41 -0300

    Added background music to main scene

---
+++ Assets/Scripts/Sound/MusicSelector.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSelector : MonoBehaviour {
+
+	public AudioClip[] musicFiles;
+	private AudioSource mainMusic;
+	private int currentFile;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mainMusic = this.gameObject.GetComponent<AudioSource>();
+		currentFile = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*
+		if(Input.GetMouseButtonDown(0))
+	    {
+	        mainMusic.Stop();
+	    }
+	    */
+
+		if(!mainMusic.isPlaying)
+		{
+			currentFile = getNewClip();
+			mainMusic.clip = musicFiles[currentFile];
+			mainMusic.Play();
+		}
+	}
+
+	private int getNewClip()
+	{
+		int newFile;
+		do
+		{
+			newFile = Random.Range(0, musicFiles.Length);
+		} while(newFile == currentFile);
+		return newFile;
+	}
+
+}
