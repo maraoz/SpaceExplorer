9e77994e52a8c65447e1e42c4b4a5ae150d5f8a7
Manuel Aráoz <manuelaraoz@gmail.com>
2014-12-13 15:52:51 -0300

    starting with grabber

---
+++ Assets/Scripts/GrabScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabScript : MonoBehaviour {
+
+    RaycastHit hit;
+    int layerMask = ~(1 << 9);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            float distance = 500f;
+            Physics.Raycast(transform.position, transform.forward, out hit, distance, layerMask);
+            Debug.Log("test2 "+hit.distance);
+        }
+    
+	}
+}
