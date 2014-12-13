37fd4d269552ac16d5075fbd5dfd4cd4ac1d1d51
Maikol <miguel.deelias@gmail.com>
2014-12-13 16:56:43 -0300

    first explotion

---
+++ Assets/Script Explotion/ExplosionHelper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionHelper : MonoBehaviour {
+
+	public static ExplosionHelper Instance;
+
+  public ParticleSystem effectYellow;
+  public ParticleSystem effectRed;
+
+	void Awake () {
+		if (Instance != null) {
+			Debug.LogError("Multiple instances of ExplotionHelper!");
+		}
+
+		Instance = this;
+	}
+	
+	public void Explosion(Vector3 position) {
+		instantiate(effectRed, position);
+		instantiate(effectYellow, position);
+	}
+
+	private ParticleSystem instantiate(ParticleSystem prefab, Vector3 position) {
+		ParticleSystem newParticleSystem = Instantiate(
+			prefab,
+			position,
+			Quaternion.identity
+		) as ParticleSystem;
+
+		Destroy(
+			newParticleSystem.gameObject,
+			newParticleSystem.startLifetime
+		);
+
+		return newParticleSystem;
+	}
+}
---
+++ Assets/Script Explotion/ExplosionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionScript : MonoBehaviour {
+
+	public float movementSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+	}
+
+	void OnCollisionEnter() {
+		ExplosionHelper.Instance.Explosion(transform.position);
+		Destroy(gameObject);
+	}
+}
